a4b26eb4936ca374e4a613f07aa8a2b79ccb3195
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-07-10 00:03:48 +0530

    Create Whatsapp.py

---
+++ Whatsapp.py
@@ -0,0 +1,33 @@
+# Whatsapp Message Sending Bot using Selenium
+# Library Used: selenium and time
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome('C:/Users/hp/Desktop/chromedriver.exe')
+driver.implicitly_wait(10)
+driver.maximize_window()
+
+driver.get("https://web.whatsapp.com/")
+
+print("Scan QR Code!")
+
+name = input("Enter Name: ")
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+
+msg = input("Enter Message: ")
+
+count = int(input("Enter Count: "))
+
+for i in range(count):
+	msg_box = driver.find_element_by_class_name("_3u328").send_keys(msg)
+	btn = driver.find_element_by_class_name("_3M-N-")
+	btn.click()
+
+time.sleep(5)
+
+driver.close()
+driver.quit()
+
+print("Test Completed!")
