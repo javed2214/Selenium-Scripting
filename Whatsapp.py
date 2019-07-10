f9697c8a43e1e0743e380cb58468dbe52ccb7d2c
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-07-10 13:01:48 +0530

    Update Whatsapp.py

--- Whatsapp.py
+++ Whatsapp.py
@@ -12,23 +12,32 @@ driver.get("https://web.whatsapp.com/")
 
 print("Scan QR Code!")
 
-name = input("Enter Name: ")
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
+x = int(input("Enter Number of Recipients: "))
+
+print("Enter Names: ")
+
+nameList=[]
+
+for i in range(x):
+	p = input()
+	nameList.append(p)
 
 msg = input("Enter Message: ")
-
 count = int(input("Enter Count: "))
 
-for i in range(count):
-	msg_box = driver.find_element_by_class_name("_3u328").send_keys(msg)
-	btn = driver.find_element_by_class_name("_3M-N-")
-	btn.click()
+for name in nameList:
+
+	user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+	user.click()
+
+	for i in range(count):
+		msg_box = driver.find_element_by_class_name("_3u328").send_keys(msg)
+		btn = driver.find_element_by_class_name("_3M-N-")
+		btn.click()
 
 time.sleep(5)
 
 driver.close()
 driver.quit()
 
-print("Message Sent!")
-
+print("Test Completed!")
